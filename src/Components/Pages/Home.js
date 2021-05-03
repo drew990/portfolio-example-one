51cb5a60ef6af115112a1128c4a7a70c23ad37fb
Andrew Banagas <andrewbanagas40@users.noreply.github.com>
2021-05-03 16:21:54 -0700

    Update 2.1.2
    Bug Fixes

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,46 +1,59 @@
 import "../../App.css";
 import "./Home.css";
-
+import PropagateLoader from "react-spinners/PropagateLoader";
+import React, { useState, useEffect } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import { Link } from "react-router-dom";
 
 function Home() {
+  const [loading, setloading] = useState(false);
+
+  useEffect(() => {
+    setloading(true);
+    setTimeout(() => {
+      setloading(false);
+    });
+  }, []);
   return (
     <>
-      <Row>
-        <Col lg className="col-lg-Home">
-          <Jumbotron fluid className="Main-img"></Jumbotron>
-        </Col>
-        <Col lg className="col-lg-Home-Sec">
-          <Jumbotron fluid className="Filler-background">
-            <h1 className="header">
-              Welcome to my Portfolio. <br />
-              My Name is Diana Brita
-            </h1>
-            <h2 className="Sub-Contact">Sweden, Stockholm</h2>
-            <h3 className="Sub-Contact">Freelancer Model</h3>
-            <h3 className="Sub-Contact">Example@gmail.com</h3>
-            <Row>
-              <Col>
-                <h3 className="hover-underline-animation">
-                  <Link className="link" to="/gallery">
-                    Gallery
-                  </Link>
-                </h3>
-              </Col>
-              <Col className="link">
-                <h3 className="hover-underline-animation">
-                  <Link className="link" to="/about">
-                    About
-                  </Link>
-                </h3>
-              </Col>
-            </Row>
-          </Jumbotron>
-        </Col>
-      </Row>
+      {loading ? (
+        <PropagateLoader size={15} color={"#000"} loading={loading} />
+      ) : (
+        <Row>
+          <Col lg className="col-lg-Home">
+            <Jumbotron fluid className="Main-img"></Jumbotron>
+          </Col>
+          <Col lg className="col-lg-Home-Sec">
+            <Jumbotron fluid className="Filler-background">
+              <h1 className="header">
+                Welcome to my Portfolio. <br />
+                My Name is Diana Brita
+              </h1>
+              <h2 className="Sub-Contact">Sweden, Stockholm</h2>
+              <h3 className="Sub-Contact">Freelancer Model</h3>
+              <h3 className="Sub-Contact">Example@gmail.com</h3>
+              <Row>
+                <Col>
+                  <h3 className="hover-underline-animation">
+                    <Link className="link" to="/gallery">
+                      Gallery
+                    </Link>
+                  </h3>
+                </Col>
+                <Col className="link">
+                  <h3 className="hover-underline-animation">
+                    <Link className="link" to="/about">
+                      About
+                    </Link>
+                  </h3>
+                </Col>
+              </Row>
+            </Jumbotron>
+          </Col>
+        </Row>
+      )}
     </>
   );
 }
